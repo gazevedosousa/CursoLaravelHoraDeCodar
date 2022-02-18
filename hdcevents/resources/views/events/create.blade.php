0f2b12ecaa416fa5a73e73d5aa72f93ca49a6ba8
unknown <gazevedosousa@hotmail.com>
2022-02-18 19:35:17 -0300

    Finalizou pagina criação de eventos e guardar no DB

--- hdcevents/resources/views/events/create.blade.php
+++ hdcevents/resources/views/events/create.blade.php
@@ -4,6 +4,31 @@
 
 @section('content')
 
-    <h1>Crie um Evento</h1>
+<div id="event-create-container" class="col-md-6 offset-md-3">
+    <h1>Crie seu Evento</h1>
+    <form action="/events" method="POST">
+        @csrf
+        <div class="form-group mb-3">
+            <label for="title" class="mb-2">Evento:</label>
+            <input type="text" class="form-control" id="title" name="title" placeholder="Nome do Evento">
+        </div>
+        <div class="form-group mb-3">
+            <label for="city" class="mb-2">Cidade:</label>
+            <input type="text" class="form-control" id="city" name="city" placeholder="Local do Evento">
+        </div>
+        <div class="form-group mb-3">
+            <label for="private" class="mb-2">O evento é privado?</label>
+            <select name="private" id="private" class="form-control">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group mb-3">
+            <label for="description" class="mb-2">Evento:</label>
+            <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+        </div>
+        <input type="submit" class ="btn btn-primary" value="Criar Evento">
+    </form>
+</div>
     
 @endsection
